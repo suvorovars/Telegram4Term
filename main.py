217a9b02f627fca782b4e3c0945ad1371200bfe5
suvorovars <suvorovars@gmail.com>
2022-06-14 18:03:53 +0500

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import npyscreen
+
+class App(npyscreen.StandardApp):
+    def onStart(self):
+        self.addForm("MAIN", MainForm, name="Telegram4Term")
+
+class MainForm(npyscreen.ActionForm):
+    # Конструктор
+    def create(self):
+        # Добавляем виджет TitleText на форму
+        self.title = self.add(npyscreen.TitleText, name="TitleText", value="Hello World!")
+    # переопределенный метод, срабатывающий при нажатии на кнопку «ok»
+    def on_ok(self):
+        self.parentApp.setNextForm(None)
+    # переопределенный метод, срабатывающий при нажатии на кнопку «cancel»
+    def on_cancel(self):
+        self.title.value = "Hello World!"
+
+MyApp = App()
+MyApp.run()
